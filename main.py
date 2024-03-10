cf44c8870c55bd57acef2aad612beec9ea75acfe
Manuel <Manuel@Este-portatil-es-de-manuel.local>
2024-03-10 17:59:12 +0100

    record audio and add it to the game

--- main.py
+++ main.py
@@ -2,42 +2,11 @@ from openai import OpenAI
 from dotenv import load_dotenv
 from chat import Chat
 import os
+import record_voice 
 
 
 load_dotenv()
 client = OpenAI(api_key = os.environ.get('API_KEY'))
-
-prompt1 = """
-    Al usuario le han dado el enunciado de un historia a medio terminar.
-    Tu trabajo es responder preguntas de si o no que te hara el usuario para tratar de adivinar la historia.
-    Solo pueder responder preguntas de si o no.
-"""
-
-prompt2 = """
-    Tu respuesta sera un JSON:
-    {
-        "reasoning": "" #Razonamiento de tus respuestas
-        "reasoning_yes_or_no_question": # Razonamiento de porque la pregunta se puede responder con si o con no
-        "yes_or_no": "" #La respuesta a la pregunta del usuario, no pongas nada si la pregunta del usuario no se puede responder con 'si' o 'no'
-    }
-    <historia> Una mujer habia inventado un motor que funcionaba con agua, esto significa el fin de la industria del petroleo.
-    Las multinacionales petroleras le habian ofrecido un monton de dinero por olvidarse de su invento.
-    Pero la mujer se nego, asi que las multinacionales recurrieron al asesinato de la mujer</historia>
-"""
-
-prompt3 = """
-    Tendras que averiguar si las preguntas de un usuario han adivinado ciertos puntos de una historia
-    Tu respuesta sera un JSON:
-    {
-        "reasoning": "" #Razonamiento de tus respuestas
-        "water_engine": "" #Pon True si el usuario ha adivinado especificamente que la mujer sabe como construir un motor de agua, en caso contrario, False
-        "oil_industries_threat": "" #Pon True si el usuario ha adivinado que la invencion era una amenaza para las industrias petroleras, en caso contrario, False
-        "oil_industries_murder": "" #Pon True si el usuario ha adivinado que las industrias petroleras mataron a la mujer, en caso contrario, False
-    }
-    <historia> Una mujer habia inventado un motor que funcionaba con agua, esto significa el fin de la industria del petroleo.
-    Las multinacionales petroleras le habian ofrecido un monton de dinero por olvidarse de su invento.
-    Pero la mujer se nego, asi que las multinacionales recurrieron al asesinato de la mujer </historia>
-"""
 
 
 
@@ -53,17 +22,48 @@ def view_achievements(json_response: dict):
     if facts_achieved >= len(json_response)-1: return True
     return False
 
-print("Bienvenido a las black stories")
+def start(story, audio):
+    prompt1 = """
+        Al usuario le han dado el enunciado de un historia a medio terminar.
+        Tu trabajo es responder preguntas de si o no que te hara el usuario para tratar de adivinar la historia.
+        Solo pueder responder preguntas de si o no.
+    """
+    prompt2 = """
+        Tu respuesta sera un JSON:
+        {
+            "reasoning": "" #Razonamiento de tus respuestas
+            "reasoning_yes_or_no_question": # Razonamiento de porque la pregunta se puede responder con si o con no
+            "yes_or_no": "" #La respuesta a la pregunta del usuario, no pongas nada si la pregunta del usuario no se puede responder con 'si' o 'no'
+        }
+        <historia>STORY</historia>
+    """.replace('STORY', story[2])
 
-chat_yes_no: Chat = Chat(client, prompt1, prompt2)
-chat_achievements: Chat = Chat(client, prompt3)
-
-while True:
-    question = input('Pregunta: ')
+    prompt3 = """
+        Tendras que averiguar si las preguntas de un usuario han adivinado ciertos puntos de una historia
+        Tu respuesta sera un JSON:
+        {
+            "reasoning": "" #Razonamiento de tus respuestas
+            "water_engine": "" #Pon True si el usuario ha adivinado especificamente que la mujer sabe como construir un motor de agua, en caso contrario, False
+            "oil_industries_threat": "" #Pon True si el usuario ha adivinado que la invencion era una amenaza para las industrias petroleras, en caso contrario, False
+            "oil_industries_murder": "" #Pon True si el usuario ha adivinado que las industrias petroleras mataron a la mujer, en caso contrario, False
+        }
+        <historia>STORY</historia>
+    """.replace('STORY', story[2])
     
-    yes_or_no(chat_yes_no.send_message(question))
-    achievements = chat_achievements.send_message(question)
-    print(achievements)
-    if view_achievements(achievements): break
+    
+    chat_yes_no: Chat = Chat(client, prompt1, prompt2)
+    chat_achievements: Chat = Chat(client, prompt3)
+    print(story[1])
+    while True:
+        if audio == 'false':
+            question = input('Pregunta: ')
+        else:
+            input('Pulsa enter para grabar')
+            question = record_voice.grabar()
+        print(question)
+        yes_or_no(chat_yes_no.send_message(question))
+        achievements = chat_achievements.send_message(question)
+        print(achievements)
+        if view_achievements(achievements): break
 
-print('Has adivinado la historia correctamente!!!')
+    print('Has adivinado la historia correctamente!!!')
---
+++ record_voice.py
@@ -0,0 +1,53 @@
+import pyaudio
+import wave
+from openai import OpenAI
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+def grabar():
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 1
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 5
+    filename = "output.wav"
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+    client = OpenAI(api_key = os.environ.get('API_KEY'))
+
+    audio_file= open("output.wav", "rb")
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file
+    )
+    return transcript.text
